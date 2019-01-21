f52770bc6a30d200aadfc6be2a8596573ceba072
agat-solutions <34028655+agat-solutions@users.noreply.github.com>
2019-01-21 15:24:41 +0100

    Update textes.js
    modif portail web carto

--- txt/textes.js
+++ txt/textes.js
@@ -4,7 +4,7 @@ var auditTxt = "Nous vous accompagnons pour la gestion optimisée de vos donnée
 var urbaTxt = "Dématérialisation de vos PLU(i) ou autres documents d'urbanisme, au format SIG respectant la norme INSPIRE, et téléversement sur le Géoportail de l'Urbanisme<br><br><a href='http://www.conformite-geoportail-urbanisme.fr/' target='_blank'>http://www.conformite-geoportail-urbanisme.fr/</a>";
 var formaTxt = "Formations QGIS métier : des programmes sur-mesure thématiques pour les ingénieurs territoriaux, bureaux d'études, services techniques"<br>"Des supports et programmes personnalisés adaptés à votre thématique métier<br>Des formateurs déclarés avec une expérience reconnue depuis plusieurs années : nous sommes intervenus pour l'INAO, IdeoBFC, le PNR du Haut-Jura, la chambre d'agriculture de la Côte d'Or, des bureaux d'études privés..."<br> "Exercices intéressants et nombreux" "Convivialité et bonne ambiance" "Qualité des données et des otuils utilisés" 
 var integrTxt = "Enrichissez votre SIG en intégrant les données métier de votre quotidien";
-var portailTxt = "Mettez en valeur votre territoire avec un portail web cartographique";
+var portailTxt = "Mettez en valeur votre territoire et vos données avec un portail web cartographique original et personnalisé"<br>"Les principales fonctionnalités d'un SIG pour les non-géomaticiens";
 var reglemTxt = "Dans le cadre du partenariat national Carto-réseaux, confiez-nous la numérisation de vos réseaux et leur téléversement sur le portail de l'INERIS <a href='http://www.carto-reseaux.fr' target='_blank'>http://www.carto-reseaux.fr</a>";
 
 
